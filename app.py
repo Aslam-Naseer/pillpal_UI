bd414ed43b5e30d384c751696c389c41addec381
Aslam-Naseer <aslamnaseer22@gmail.com>
2024-02-02 13:50:10 +0530

    Change to neurodisease detection

--- app.py
+++ app.py
@@ -21,9 +21,14 @@ def index():
     # Use the 'select' method to retrieve all data from 'buckets'
     response_bucket = supabase.table('buckets').select('*').execute()
     response_medicine = supabase.table('medicines').select('*').execute()
+
+    patients_list = ["p1","p2","p3"]
+    images_list = [123,234,345,456,567]
+
     print("Response", response_bucket.data)
     print("Response M", response_medicine.data)
-    return render_template('index.html', data_bucket=response_bucket.data, data_medicine=response_medicine.data)
+    # return render_template('index.html', data_bucket=response_bucket.data, data_medicine=response_medicine.data)
+    return render_template('index.html', patients=patients_list, images = images_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
