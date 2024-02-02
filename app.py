bc04d47d53c86a1f5d3101c8197fdf2b8aa74c0a
Aslam-Naseer <aslamnaseer22@gmail.com>
2024-02-02 15:10:10 +0530

    Set up image list

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
-from supabase import create_client, Client
+# from supabase import create_client, Client
+
+
 
 load_dotenv()
 app = Flask(__name__)
@@ -14,19 +16,19 @@ print(key)
 if not url or not key:
     raise ValueError("Supabase credentials not found in .env file")
 
-supabase = create_client(url, key)
+# supabase = create_client(url, key)
 
 @app.route('/')
 def index():
     # Use the 'select' method to retrieve all data from 'buckets'
-    response_bucket = supabase.table('buckets').select('*').execute()
-    response_medicine = supabase.table('medicines').select('*').execute()
+    # response_bucket = supabase.table('buckets').select('*').execute()
+    # response_medicine = supabase.table('medicines').select('*').execute()
 
     patients_list = ["pdsda1","p2","p3"]
-    images_list = [123,234,345,456,567]
+    images_list = ["a",12,2,3,4,4,4]
 
-    print("Response", response_bucket.data)
-    print("Response M", response_medicine.data)
+    # print("Response", response_bucket.data)
+    # print("Response M", response_medicine.data)
     # return render_template('index.html', data_bucket=response_bucket.data, data_medicine=response_medicine.data)
     return render_template('index.html', patients=patients_list, images = images_list)
 
